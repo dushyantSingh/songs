a36a9f007e9509e0d8a20afc0a2d4132930c2caf
Dushyant Singh <dushyantsingh1097@gmail.com>
2022-03-14 13:46:39 +0800

    Added theme

---
+++ Songs/Songs/Theme/Theme.swift
@@ -0,0 +1,27 @@
+//
+//  Theme.swift
+//  Songs
+//
+//  Created by Dushyant Singh on 14/3/22.
+//
+
+import Foundation
+import UIKit
+
+struct Theme {
+    struct Color {
+        static let brickRedColor = UIColor(named: "brickRedColor")
+        static let primaryColor = UIColor(named: "primaryColor")
+    }
+    struct Font {
+        static func thinFont(with size: CGFloat) -> UIFont {
+            return UIFont(name: "HelveticaNeue-Thin", size: size)!
+        }
+        static func boldFont(with size: CGFloat) -> UIFont {
+            return UIFont(name: "HelveticaNeue-Medium", size: size)!
+        }
+        static func regularFont(with size: CGFloat) -> UIFont {
+            return UIFont(name: "HelveticaNeue", size: size)!
+        }
+    }
+}
