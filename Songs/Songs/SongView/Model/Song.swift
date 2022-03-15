cbb9ae8bb04678de34e1ba9de678b9e5713717b5
Dushyant Singh <dushyantsingh1097@gmail.com>
2022-03-15 13:27:15 +0800

    Added Songs model

---
+++ Songs/Songs/SongView/Model/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  Songs
+//
+//  Created by Dushyant Singh on 15/3/22.
+//
+
+import Foundation
+struct Song {
+    let id: String
+    let name: String
+    let audioURL: String
+    var songStatus: SongStatus
+}
---
+++ Songs/Songs/SongView/Model/SongStatus.swift
@@ -0,0 +1,16 @@
+//
+//  SongStatus.swift
+//  Songs
+//
+//  Created by Dushyant Singh on 15/3/22.
+//
+
+import Foundation
+
+enum SongStatus {
+    case availableToDownload
+    case downloading
+    case downloaded
+    case playing
+    case paused
+}
